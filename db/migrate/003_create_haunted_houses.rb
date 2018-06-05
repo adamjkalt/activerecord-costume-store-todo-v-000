7b37fa53c0c4d283082316b728e2baf6345c47a9
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-05 18:40:10 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,6 +3,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
+      t.string :name
+      t.string :name
+      t.string :name
     end
   end
-end
+  end
