c87a123ca513d98f04a83f8ea1aa81031792674d
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-05 18:42:38 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,9 +3,16 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costumestores do |t|
       t.string :name
-      t.string :name
-      t.string :name
-      t.string :name
+      t.string :location
+      t.string :costume_inventory
+      t.string :employees
+      t.string :in_business
+      t.string :opening_time
+      t.string :closing_time
+
+
     end
   end
   end
+  opening time
+  closing time
