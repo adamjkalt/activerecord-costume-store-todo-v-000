3548927ff678e92571cb71e85f73d8efd9b45cc1
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-05 18:40:37 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,7 +1,10 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
 
   def change
-    create_table :students do |t|
+    create_table :costumes do |t|
+      t.string :name
+      t.string :name
+      t.string :name
       t.string :name
     end
   end
